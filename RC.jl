b8ec45aff19c1fc27a7786a2b178970d2948e283
chrisharby <c.harbord@hotmail.co.uk>
2020-04-24 00:47:44 +0100

    Update RC.jl

--- RC.jl
+++ RC.jl
@@ -4,14 +4,15 @@ nx = 200
 rng = LinRange(-2,1,nx)
 lc =  0.1#10 .^rng#1
 θ = π/4#LinRange(0,π/2,nx)
-σ1 = -1
+σ1 = -100
 KI = zeros(nx)
 KII = zeros(nx)
-μ = 0.2
+μ = -1
 #Kout  = straightcrack(lc[1],γ,θ,σ1)
 for i = 1:nx
     Kout  = straightcrack(lc,γ[i],θ,σ1,μ)
     KII[i] = real(Kout[1])
     KI[i] = imag(Kout[1])
 end
-plot(γ/π,KII./((abs(σ1))*sqrt(π/lc)))
+
+plot!(γ/π,KI./maximum(abs.(KI)))
